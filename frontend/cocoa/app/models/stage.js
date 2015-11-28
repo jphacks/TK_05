128314d86e5beb8e29f49bfca4d23cc97ef07684
Nomuken <nomuken@spicadoll.com>
2015-11-29 07:21:43 +0900

    fix field

--- frontend/cocoa/app/models/stage.js
+++ frontend/cocoa/app/models/stage.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  requiredPoints: DS.attr('number')
+  requiredPoints: DS.attr('number'),
+  requiredQuestion: DS.belongTo('question')
 });
