4caf14ab979d8cbdcedc41d771a4aaecdf6fb4cf
Nomuken <nomuken@spicadoll.com>
2015-11-28 23:47:56 +0900

    I'm palloc

--- server/sharo/sharo/serializers.py
+++ server/sharo/sharo/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from sharo import models
@@ -56,6 +58,25 @@ class AnswerSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(detail="This flag had been submitted.")
 
         return data
+
+    def create(self, validated_data):
+        user = self.context.get('request').user
+        question = validated_data['question']
+        answer = validated_data['answer']
+
+        try:
+            flag = Flag.objects.get(question=question, answer=answer)
+        except Flag.DoesNotExist:
+            pass
+
+        if flag:
+            user.last_scored = datetime.now()
+            user.save()
+
+        a = Answer(user=user, question=question, answer=answer)
+        a.save()
+
+        return a
 
 class FlagSerializer(serializers.ModelSerializer):
     class Meta:
