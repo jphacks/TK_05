743a0656c07370777ed217a2f2da562846afb16a
Nomuken <nomuken@spicadoll.com>
2015-11-29 07:23:02 +0900

    fix some probrem

--- frontend/cocoa/app/models/notice.js
+++ frontend/cocoa/app/models/notice.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    user: DS.belongsTo('user'),
-    question: DS.belongsTo('question'),
-    importance: DS.belongsTo('importance');
-    title: DS.attr('string'),
-    body: DS.attr('string')
+  user: DS.belongsTo('user'),
+  question: DS.belongsTo('question'),
+  importance: DS.belongsTo('importance'),
+  title: DS.attr('string'),
+  body: DS.attr('string')
 });
